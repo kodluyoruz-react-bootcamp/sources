472f0b6f43de5dd16aa6682430cc8daff5bdac0e
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-26 13:50:17 +0300

    react useState()

--- day3/2-react-base/src/App.js
+++ day3/2-react-base/src/App.js
@@ -1,16 +1,32 @@
-import Button from "./components/Button";
-import UserList from "./components/UserList";
+import { useState } from "react";
 
 function App() {
+	const [isVisible, setIsVisible] = useState(false);
+	const [users, setUsers] = useState([
+		{ name: "Metin", age: 22 },
+		{ name: "Hande", age: 24 },
+	]);
+
+	const handleClick = () => {
+		setUsers((prev) => [...prev, { name: "Seda", age: 28 }]);
+	};
+
+	const handleToggle = () => setIsVisible(!isVisible);
+
 	return (
-		<>
-			<Button
-				text="Click Me"
-				disabled={false}
-				styles={{ fontSize: 22, color: "#333" }}
-			/>
-			<UserList users={["Murat", "Kenan", "Ayşe"]} data={{ a: 1, b: 2 }} />
-		</>
+		<div>
+			<button onClick={handleToggle}>Toggle {isVisible ? "off" : "on"}</button>
+
+			{isVisible && (
+				<>
+					{users.map((user, i) => (
+						<li key={i}>{user.name}</li>
+					))}
+					<hr />
+					<button onClick={handleClick}>Add user</button>
+				</>
+			)}
+		</div>
 	);
 }
 
