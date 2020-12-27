2a4760b5bc8c4c802c4e5a7799b15632d478e4aa
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-27 14:47:58 +0300

    users page users list

--- day4/4-routing/src/pages/Users/index.js
+++ day4/4-routing/src/pages/Users/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function Users() {
-	return <div>Users</div>;
+	const [loading, setLoading] = useState(true);
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		axios(`${process.env.REACT_APP_API_ENDPOINT}/users`)
+			.then((res) => setUsers(res.data))
+			.finally(() => setLoading(false));
+	}, []);
+
+	return (
+		<div>
+			<h2>Users</h2>
+			{loading && <div>Loading...</div>}
+			{users.map((user) => (
+				<li key={user.id}>{user.name}</li>
+			))}
+		</div>
+	);
 }
 
 export default Users;
