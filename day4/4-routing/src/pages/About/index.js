1bac40eb112bb48bae44c1d17178892a9011f68a
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-27 14:31:30 +0300

    react router dom installation

---
+++ day4/4-routing/src/pages/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+	return (
+		<div>
+			About
+		</div>
+	)
+}
+
+export default About
---
+++ day4/4-routing/src/pages/Users/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Users() {
+	return <div>Users</div>;
+}
+
+export default Users;
