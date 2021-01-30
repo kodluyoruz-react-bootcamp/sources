6a673fe6bd6f861dd842e9701940b4092536ecc4
Mehmet Seven <mehmetseven0@gmail.com>
2021-01-30 16:37:59 +0300

    insert user endpoint

--- 1-apollo-client/src/apollo.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-	uri: "https://kodluyoruz.hasura.app/v1/graphql",
-	cache: new InMemoryCache(),
-});
-
-export default client;
--- 1-apollo-client/src/pages/Home/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-	return <div>home</div>;
-}
-
-export default Home;
